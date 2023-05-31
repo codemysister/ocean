cb7ae0b97fdb244f5eadebfbf5f20d7ccf41e480
Deva <theprojessa@gmail.com>
2023-05-31 21:21:31 +0700

    [FEAT] install template dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('admin')->name('admin.')->group(function(){
+
+        Route::get('/dashboard', AdminDashboardController::class)->name('dashboard');
+
 });
