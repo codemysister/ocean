1a2752379ffb8f400b72b77c03f516f3e17aa854
Deva <theprojessa@gmail.com>
2023-06-09 03:42:03 +0700

    [FIX] ubah helper function route untuk url 1 segment

--- app/Helpers/RouteActive.php
+++ app/Helpers/RouteActive.php
@@ -2,7 +2,11 @@
 
 function isActiveRoute($routeName)
 {
+    if (request()->segment(2) === null) {
+        $currentRoute = request()->segment(1);
+    } else {
+        $currentRoute = request()->segment(1) . '.' . request()->segment(2);
+    }
 
-    $currentRoute = request()->segment(1) .'.'. request()->segment(2);
     return $currentRoute === $routeName ? 'shadow-soft-xl bg-white rounded-lg font-semibold text-slate-700' : '';
 }
